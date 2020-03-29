e838558cad1a3a370e2e79497d99eb9f387bf839
Thingcoder1 <thingcoder1@gmail.com>
2020-03-29 18:06:40 -0500

    changed the whole of the lexer into a function

--- leaphd-code/lexer.py
+++ leaphd-code/lexer.py
@@ -19,56 +19,59 @@ lexset = [int,var,math,eql,eol,dec,opa,cpa,sep,pnt,fwa,bka]
 #the names of the token types
 nameset = ['int','var','math','eql','eol','dec','opa','cpa','sep','pnt','fwa','bka']
 
-#opening the selected .lpd document
-with open('test.lpd', 'r') as f:
-    lpd = f.read()
+def lex(filename):
+    #opening the selected .lpd document
+    with open(filename, 'r') as f:
+        lpd = f.read()
 
-#list to put the broken-down program in. Different tokens are separated in this
-lexed = []
+    #list to put the broken-down program in. Different tokens are separated in this
+    lexed = []
 
-#strings to count the characters and ints
-#ints b/c if they aren't grouped together at some point they will become x=1 5 not x=15
-charstring = ''
-intstring = ''
+    #strings to count the characters and ints
+    #ints b/c if they aren't grouped together at some point they will become x=1 5 not x=15
+    charstring = ''
+    intstring = ''
 
-#list of the chars in the .lpd document
-charlist = []
+    #list of the chars in the .lpd document
+    charlist = []
 
-#adding chars to charlist
-[charlist.append(char) for i,char in enumerate(lpd)]
+    #adding chars to charlist
+    [charlist.append(char) for i,char in enumerate(lpd)]
 
-#HUGE for loop. put respective tokens together in lexed list
-for i,char in enumerate(lpd):
-    #testing for digits to put in intlist
-    if char.isdigit() == True:
-        intstring += char
+    #HUGE for loop. put respective tokens together in lexed list
+    for i,char in enumerate(lpd):
+        #testing for digits to put in intlist
+        if char.isdigit() == True:
+            intstring += char
 
-    #testing for chars to put in charlist
-    if char.isalpha() == True:
-        charstring += char
+        #testing for chars to put in charlist
+        if char.isalpha() == True:
+            charstring += char
 
-    else:
-        #testing for digits to put in lexed
-        if char.isdigit() == False:
-            lexed.append(intstring)
-            lexed.append(charstring)
-            intstring = ''
-            charstring = ''
-            lexed.append(char)
-            charprinted = 1 #this is to tell the program not to duplicate
-                            #chars b/c it already got one in the list
+        else:
+            #testing for digits to put in lexed
+            if char.isdigit() == False:
+                lexed.append(intstring)
+                lexed.append(charstring)
+                intstring = ''
+                charstring = ''
+                lexed.append(char)
+                charprinted = 1 #this is to tell the program not to duplicate
+                                #chars b/c it already got one in the list
 
-#the list for the chars w/ labels
-parsedLex = []
+    #the list for the chars w/ labels
+    parsedLex = []
 
-#for loop
-for i in range(len(lexed)):
-    for a in range(len(lexset)):
-        #try to get rid of this to improve efficency
-        try:
-            lexset[a].parseString(lexed[i])
-            parsedLex.append((nameset[a],lexed[i]))#format of [name,char]
-        except:
-            pass
+    #for loop
+    for i in range(len(lexed)):
+        for a in range(len(lexset)):
+            #try to get rid of this to improve efficency
+            try:
+                lexset[a].parseString(lexed[i])
+                parsedLex.append((nameset[a],lexed[i]))#format of [name,char]
+            except:
+                pass
 
-print(parsedLex)#print out the list of parsed lexed chars
+    return(parsedLex)#print out the list of parsed lexed chars
+
+print(lex('test.lpd'))
