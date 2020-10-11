a8ae7e8c83dbba474789e64cdf67bf30d7199261
Ian Pratt <ian@ian-Inspiron-15-3567.attlocal.net>
2020-10-10 21:26:34 -0500

    cleaning up, adding better comments

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -16,23 +16,25 @@ func Lex(pathToFile string) []string {
 	// makes the list
 	tokenList := make([]string, 0, len(data))
 
-	// makes the string
+	// makes the string that holds the tokens
 	var charString string
 
-	// for loop to append the characters of the file to the list
+	// for loop to append the characters of the file to the list of tokens
 	for i := 0; i <= len(data)-1; i++ {
 
 		// temporarily stores the character into this value and converts it into a string
 		tempChar := string(data[i])
 
-		// checks if tempvar is a separater
+		// checks if tempvar is a separator
 		// if it isn't append the character to the string
 		if strings.ContainsAny(tempChar, " ()[]{}") != true {
 			charString = charString + tempChar
-			// if it is, don't and set charString to blank value
+
+			// if it is a space, don't and set charString to blank value
 		} else if tempChar == " " {
 			tokenList = append(tokenList, charString)
 			charString = ""
+			// if it is a separater, append the string, then separator, then make charString = ""
 		} else if strings.ContainsAny(tempChar, "()[]{}") {
 			if charString != "" {
 				tokenList = append(tokenList, charString)
@@ -48,7 +50,6 @@ func Lex(pathToFile string) []string {
 		}
 
 		// while appending, converts the datatype to string instead of byte
-		// charList = append(charList, string(data[i]))
 	}
 
 	return tokenList
